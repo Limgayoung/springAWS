794f4df5b7c7cb346bf4bf42b5f8ee73315ec7c5
Limgayoung <ygy920@gmail.com>
2022-03-10 21:50:17 +0900

    delete folder

--- Spring key principle/core/src/test/java/hello/core/scan/filter/MyIncludeComponent.java
+++
@@ -1,9 +0,0 @@
-package hello.core.scan.filter;
-
-import java.lang.annotation.*;
-
-@Target(ElementType.TYPE) //TYPE이라고 하면 class 레벨에 붙는 것
-@Retention(RetentionPolicy.RUNTIME)
-@Documented
-public @interface MyIncludeComponent {
-}

19edbdc69a43719171ed8d6792e748bfc07ebf81
Limgayoung <ygy920@gmail.com>
2022-05-13 23:36:04 +0900

    add

---
+++ src/main/java/com/springAWS/domain/posts/Posts.java
@@ -0,0 +1,32 @@
+package com.springAWS.domain.posts;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class Posts { //실제 DB 테이블과 매칭될 클래스, 보통 Entity 클래스라고 함
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //PK의 생성 규칙, GenerationType.IDENTITY 를 해야 auto_increment 됨
+    private Long id;
+
+    @Column(length = 500, nullable = false) //테이블의 칼럼 //선언 안해도 해당 클래스의 필드는 모두 칼럼이 됨
+    private String title;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
+
+    private String author;
+
+    @Builder
+    public Posts(String title, String content, String author) {
+        this.title = title;
+        this.content = content;
+        this.author = author;
+    }
+}

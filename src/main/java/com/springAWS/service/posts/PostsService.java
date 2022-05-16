12f63263e53e838522c444a202c19729933d284c
Limgayoung <ygy920@gmail.com>
2022-05-16 22:46:01 +0900

    add

---
+++ src/main/java/com/springAWS/service/posts/PostsService.java
@@ -0,0 +1,38 @@
+package com.springAWS.service.posts;
+
+import com.springAWS.domain.posts.Posts;
+import com.springAWS.domain.posts.PostsRepository;
+import com.springAWS.web.dto.PostsResponseDto;
+import com.springAWS.web.dto.PostsSaveRequestDto;
+import com.springAWS.web.dto.PostsUpdateRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor //final이 선언된 모든 필드를 인자값으로 하는 생성자 생성해줌
+@Service
+public class PostsService {
+
+    private final PostsRepository postsRepository;
+
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+
+        return id;
+    }
+
+    public PostsResponseDto findById(Long id) {
+        Posts entity = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        return new PostsResponseDto(entity);
+    }
+}
